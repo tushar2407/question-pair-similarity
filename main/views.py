5015ca26fbb52d783de8273624b7c857cabeb7ca
Tushar Mohan <tushar.mohan2001@gmail.com>
2021-11-20 20:20:45 +0530

    basic skeleton done

--- main/views.py
+++ main/views.py
@@ -4,3 +4,13 @@ from django.shortcuts import render
 
 def home(request):
     return render(request, 'main/home.html')
+
+def test(request):
+    result = {}
+    
+    if request.method == 'POST':
+        question_1 = request.POST.get('question_1', '')
+        question_2 = request.POST.get('question_2', '')
+        print(question_2, question_1)
+    
+    return render(request, 'main/test.html', context=result)
