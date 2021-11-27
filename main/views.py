82eaa7b71f94db1d537a554d660a2b457a13d170
Tushar Mohan <tushar.mohan2001@gmail.com>
2021-11-28 02:07:40 +0530

    progress

--- main/views.py
+++ main/views.py
@@ -1,4 +1,10 @@
 from django.shortcuts import render
+from main.utils import (
+    uni_lr, uni_xg,
+    bi_lr, bi_xg,
+    tri_lr, tri_xg,
+    bow_xg
+)
 
 # Create your views here.
 
@@ -12,5 +18,11 @@ def test(request):
         question_1 = request.POST.get('question_1', '')
         question_2 = request.POST.get('question_2', '')
         print(question_2, question_1)
+        result['uni_lr'] = uni_lr(question_1, question_2)
+        result['uni_xg'] = uni_xg(question_1, question_2)
+        result['bi_lr'] = bi_lr(question_1, question_2)
+        result['bi_xg'] = bi_xg(question_1, question_2)
+        result['tri_lr'] = tri_lr(question_1, question_2)
+        result['tri_xg'] = tri_xg(question_1, question_2)
     
     return render(request, 'main/test.html', context=result)
