d0d68b97ad65bb13af47c9bf4f90af2580d9c6ef
Tushar Mohan <tushar.mohan2001@gmail.com>
2021-11-28 05:29:30 +0530

    testing

--- main/utils.py
+++ main/utils.py
@@ -1,12 +1,13 @@
-# import scipy
+import scipy
 import pickle
 from question_similarity.settings import BASE_DIR
 import re
 from string import punctuation
-from keras.preprocessing.sequence import pad_sequences
-from keras.models import load_model
+# from keras.preprocessing.sequence import pad_sequences
+# from keras.models import load_model
 import pandas as pd
 import numpy as np
+
 
 def pad_str(s):
     return ' '+s+' '
@@ -25,73 +26,73 @@ def normalize_text(text):
 
     return text
 
-# def get_tfidf(q1, q2, ngram="unigram"):
-#     tfidf = pickle.load(open(f"{BASE_DIR}/models/analysis1/{ngram}.pkl", "rb"))
-#     q1ngram_trans = tfidf.transform([q1, ])
-#     q2ngram_trans = tfidf.transform([q2, ])
-#     return scipy.sparse.hstack((q1ngram_trans, q2ngram_trans))
+def get_tfidf(q1, q2, ngram="unigram"):
+    tfidf = pickle.load(open(f"{BASE_DIR}/models/analysis1/{ngram}.pkl", "rb"))
+    q1ngram_trans = tfidf.transform([q1, ])
+    q2ngram_trans = tfidf.transform([q2, ])
+    return scipy.sparse.hstack((q1ngram_trans, q2ngram_trans))
 
-# def bow_xg(q1, q2):
-#     CV = pickle.load(open(f"{BASE_DIR}/models/analysis1/bow.pkl", "rb"))
-#     q1_trans = CV.transform([q1,])
-#     q2_trans = CV.transform([q2,])
-#     X = scipy.sparse.hstack((q1_trans, q2_trans))
-#     classifier1 = pickle.load(open(f"{BASE_DIR}/models/analysis1/bow_xg.pkl", "rb"))
-#     return classifier1.predict(X)
+def bow_xg(q1, q2):
+    CV = pickle.load(open(f"{BASE_DIR}/models/analysis1/bow.pkl", "rb"))
+    q1_trans = CV.transform([q1,])
+    q2_trans = CV.transform([q2,])
+    X = scipy.sparse.hstack((q1_trans, q2_trans))
+    classifier1 = pickle.load(open(f"{BASE_DIR}/models/analysis1/bow_xg.pkl", "rb"))
+    return classifier1.predict(X)
 
-# def uni_xg(q1, q2): 
-#     X = get_tfidf(q1, q2)
-#     classifier2 =pickle.load(open(f"{BASE_DIR}/models/analysis1/classfier2.dat", "rb"))
-#     return classifier2.predict(X)
+def uni_xg(q1, q2): 
+    X = get_tfidf(q1, q2)
+    classifier2 =pickle.load(open(f"{BASE_DIR}/models/analysis1/classfier2.dat", "rb"))
+    return classifier2.predict(X)
 
-# def bi_xg(q1, q2):
-#     X = get_tfidf(q1, q2, "bigram")
-#     classifier3 = pickle.load(open(f"{BASE_DIR}/models/analysis1/classfier3.dat", "rb"))
-#     return classifier3.predict(X)
+def bi_xg(q1, q2):
+    X = get_tfidf(q1, q2, "bigram")
+    classifier3 = pickle.load(open(f"{BASE_DIR}/models/analysis1/classfier3.dat", "rb"))
+    return classifier3.predict(X)
 
-# def tri_xg(q1, q2):
-#     X = get_tfidf(q1, q2, "trigram")
-#     classifier4 = pickle.load(open(f"{BASE_DIR}/models/analysis1/classfier4.dat", "rb"))
-#     return classifier4.predict(X)
+def tri_xg(q1, q2):
+    X = get_tfidf(q1, q2, "trigram")
+    classifier4 = pickle.load(open(f"{BASE_DIR}/models/analysis1/classfier4.dat", "rb"))
+    return classifier4.predict(X)
 
-# def uni_lr(q1, q2):
-#     X = get_tfidf(q1, q2)
-#     clf = pickle.load(open(f"{BASE_DIR}/models/analysis3/lr_unigram_unprocessed.pkl", "rb"))
-#     return clf.predict(X)
+def uni_lr(q1, q2):
+    X = get_tfidf(q1, q2)
+    clf = pickle.load(open(f"{BASE_DIR}/models/analysis3/lr_unigram_unprocessed.pkl", "rb"))
+    return clf.predict(X)
 
-# def bi_lr(q1, q2):
-#     X = get_tfidf(q1, q2, "bigram")
-#     clf = pickle.load(open(f"{BASE_DIR}/models/analysis3/lr_bigram_unprocessed.pkl", "rb"))
-#     return clf.predict(X)
+def bi_lr(q1, q2):
+    X = get_tfidf(q1, q2, "bigram")
+    clf = pickle.load(open(f"{BASE_DIR}/models/analysis3/lr_bigram_unprocessed.pkl", "rb"))
+    return clf.predict(X)
 
-# def tri_lr(q1, q2):
-#     X = get_tfidf(q1, q2, "trigram")
-#     clf = pickle.load(open(f"{BASE_DIR}/models/analysis3/lr_trigram_unprocessed.pkl", "rb"))
-#     return clf.predict(X)
+def tri_lr(q1, q2):
+    X = get_tfidf(q1, q2, "trigram")
+    clf = pickle.load(open(f"{BASE_DIR}/models/analysis3/lr_trigram_unprocessed.pkl", "rb"))
+    return clf.predict(X)
     
 
 # # ## check 
-def create_padded_seqs(texts, max_len=10):
-    counts_vectorizer = pickle.load(open(f"{BASE_DIR}/models/analysis2/bow.pkl", "rb"))
-    other_index = len(counts_vectorizer.vocabulary_)
-    words_tokenizer = re.compile(counts_vectorizer.token_pattern)
-    seqs = list(map(lambda s: 
-            [
-                counts_vectorizer.vocabulary_[w] if w in counts_vectorizer.vocabulary_ else other_index
-                for w in words_tokenizer.findall(s.lower())
-            ], texts))
-    # texts.apply(
-    #     lambda s: 
-    #         [
-    #             counts_vectorizer.vocabulary_[w] if w in counts_vectorizer.vocabulary_ else other_index
-    #             for w in words_tokenizer.findall(s.lower())
-    #         ]
-    #     )
-    return pad_sequences(seqs, maxlen=max_len)
+# def create_padded_seqs(texts, max_len=10):
+#     counts_vectorizer = pickle.load(open(f"{BASE_DIR}/models/analysis2/bow.pkl", "rb"))
+#     other_index = len(counts_vectorizer.vocabulary_)
+#     words_tokenizer = re.compile(counts_vectorizer.token_pattern)
+#     seqs = list(map(lambda s: 
+#             [
+#                 counts_vectorizer.vocabulary_[w] if w in counts_vectorizer.vocabulary_ else other_index
+#                 for w in words_tokenizer.findall(s.lower())
+#             ], texts))
+#     # texts.apply(
+#     #     lambda s: 
+#     #         [
+#     #             counts_vectorizer.vocabulary_[w] if w in counts_vectorizer.vocabulary_ else other_index
+#     #             for w in words_tokenizer.findall(s.lower())
+#     #         ]
+#     #     )
+#     return pad_sequences(seqs, maxlen=max_len)
 
-def lstm_mlp(q1, q2):
-    model = load_model(f"{BASE_DIR}/models/analysis2/model.h5")
-    x1 = create_padded_seqs(np.array([q1,]))
-    x2 = create_padded_seqs(np.array([q2,]))
-    y_pred = model.predict([x1, x2],128)
-    return np.where(y_pred>0.5, 1, 0)
+# def lstm_mlp(q1, q2):
+#     model = load_model(f"{BASE_DIR}/models/analysis2/model.h5")
+#     x1 = create_padded_seqs(np.array([q1,]))
+#     x2 = create_padded_seqs(np.array([q2,]))
+#     y_pred = model.predict([x1, x2],128)
+#     return np.where(y_pred>0.5, 1, 0)
--- main/views.py
+++ main/views.py
@@ -1,10 +1,11 @@
 from django.shortcuts import render
 from main.utils import (
-    # uni_lr, uni_xg,
-    # bi_lr, bi_xg,
-    # tri_lr, tri_xg,
-    # bow_xg, 
-    normalize_text, lstm_mlp
+    uni_lr, uni_xg,
+    bi_lr, bi_xg,
+    tri_lr, tri_xg,
+    bow_xg, 
+    normalize_text, 
+    # lstm_mlp
 )
 
 # Create your views here.
@@ -18,12 +19,12 @@ def test(request):
     if request.method == 'POST':
         question_1 = normalize_text( request.POST.get('question_1', ''))
         question_2 = normalize_text( request.POST.get('question_2', ''))
-        # result['Unigram with Logistic Regression'] = uni_lr(question_1, question_2)[0]
-        # result['Unigram with XGBoost'] = uni_xg(question_1, question_2)[0]
-        # result['Bigram with Logistic Regression'] = bi_lr(question_1, question_2)[0]
-        # result['Bigram with XGBoost'] = bi_xg(question_1, question_2)[0]
-        # result['Trigram with Logistic Regression'] = tri_lr(question_1, question_2)[0]
-        # result['Trigram with XGBoost'] = tri_xg(question_1, question_2)[0]
-        result['LSTM with MLP'] = lstm_mlp(question_1, question_2)[0][0]
+        result['Unigram with Logistic Regression'] = uni_lr(question_1, question_2)[0]
+        result['Unigram with XGBoost'] = uni_xg(question_1, question_2)[0]
+        result['Bigram with Logistic Regression'] = bi_lr(question_1, question_2)[0]
+        result['Bigram with XGBoost'] = bi_xg(question_1, question_2)[0]
+        result['Trigram with Logistic Regression'] = tri_lr(question_1, question_2)[0]
+        result['Trigram with XGBoost'] = tri_xg(question_1, question_2)[0]
+        # result['LSTM with MLP'] = lstm_mlp(question_1, question_2)[0][0]
 
     return render(request, 'main/test.html', context={'result':result})
